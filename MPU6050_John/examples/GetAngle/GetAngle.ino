5c03f35007b54ced42784e22df5dd24ddb21dc98
John Diamond <1334769096@qq.com>
2019-11-16 12:32:15 +0800

    simple rate is 50Hz

--- MPU6050_John/examples/GetAngle/GetAngle.ino
+++ MPU6050_John/examples/GetAngle/GetAngle.ino
@@ -3,9 +3,12 @@
 #include <Wire.h>
 
 MPU6050 mpu6050(Wire);
+double sLastime = 0;
+double sCurtime = 0;
+double sDiffTim = 0;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(57600);
   Wire.begin();
   mpu6050.begin(MPU6050_ADDR_GND);
   mpu6050.calcGyroOffsets(true,MPU6050_ADDR_GND);  
@@ -14,15 +17,29 @@ void setup() {
 }
 
 void loop() {
-  //Print 2IMUs' X Angle
-  mpu6050.update(MPU6050_ADDR_GND);
-  float AngleX_GND = mpu6050.getAngleX();
-  Serial.print("AngleX_GND_0x68 : ");
-  Serial.print(AngleX_GND);
-  Serial.print("\t");
-  
-  mpu6050.update(MPU6050_ADDR_VCC);
-  float AngleX_VCC = mpu6050.getAngleX_1();
-  Serial.print("AngleX_VCC_0x69 : ");
-  Serial.println(AngleX_VCC);
+	sCurtime = millis();
+	sDiffTim = sCurtime - sLastime;	
+	if(sDiffTim >= 20)  //Data Rate is 50Hz
+	{
+		Serial.print("Now time is :");
+		Serial.print(sCurtime);
+		Serial.print("ms\t");
+		Serial.print("Diff time is :");
+		Serial.print(sDiffTim);
+		Serial.print("ms\t");
+		
+		//Print 2IMUs' X Angle
+		mpu6050.update(MPU6050_ADDR_GND);
+		float AngleX_GND = mpu6050.getAngleX();
+		Serial.print("AngleX_GND_0x68 : ");
+		Serial.print(AngleX_GND);
+		Serial.print("\t");
+
+		mpu6050.update(MPU6050_ADDR_VCC);
+		float AngleX_VCC = mpu6050.getAngleX_1();
+		Serial.print("AngleX_VCC_0x69 : ");
+		Serial.println(AngleX_VCC);
+
+		sLastime = sCurtime;
+	}
 }
