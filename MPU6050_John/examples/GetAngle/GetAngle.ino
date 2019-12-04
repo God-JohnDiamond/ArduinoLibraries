00f31784fd21f8fa957db41e3d9d547e541ebb77
Baidong Wang <baidong.wang@simcontec.com>
2019-12-04 11:55:49 +0800

    add relay control

--- MPU6050_John/examples/GetAngle/GetAngle.ino
+++ MPU6050_John/examples/GetAngle/GetAngle.ino
@@ -6,45 +6,62 @@ MPU6050 mpu6050(Wire);
 double sLastime = 0;
 double sCurtime = 0;
 double sDiffTim = 0;
+double LstDifAngle = 0;
+double CurDifAngle = 0;
 
 void setup() {
   Serial.begin(57600);
   Wire.begin();
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
   mpu6050.begin(MPU6050_ADDR_GND);
   mpu6050.calcGyroOffsets(true,MPU6050_ADDR_GND);  
   mpu6050.begin(MPU6050_ADDR_VCC);
   mpu6050.calcGyroOffsets(true,MPU6050_ADDR_VCC);
+  digitalWrite(4, LOW);
+  digitalWrite(5, LOW);
 }
 
 void loop() {
-
-
+	mpu6050.update(MPU6050_ADDR_GND);
+	float AngleX_GND = mpu6050.getAngleX();
+	mpu6050.update(MPU6050_ADDR_VCC);
+	float AngleX_VCC = mpu6050.getAngleX_1();
+	CurDifAngle = AngleX_GND - AngleX_VCC;
+	
 	sCurtime = millis();
 	sDiffTim = sCurtime - sLastime;	
 	if(sDiffTim >= 20)  //Data Rate is 50Hz
 	{
-		Serial.print("Now time is :");
-		Serial.print(sCurtime);
-		Serial.print("ms\t");
 		Serial.print("Diff time is :");
 		Serial.print(sDiffTim);
 		Serial.print("ms\t");
 		
 		//Print 2IMUs' X Angle
-		mpu6050.update(MPU6050_ADDR_GND);
-		float AngleX_GND = mpu6050.getAngleX();
+
 		Serial.print("AngleX_GND_0x68 : ");
 		Serial.print(AngleX_GND);
 		Serial.print("\t");
 		
-		mpu6050.update(MPU6050_ADDR_VCC);
-		float AngleX_VCC = mpu6050.getAngleX_1();
 		Serial.print("AngleX_VCC_0x69 : ");
 		Serial.print(AngleX_VCC);
 		Serial.print("\t");
-		Serial.print("DiffAngel : ");
-		Serial.println(AngleX_VCC - AngleX_GND);
 
+		Serial.print("AngleX_Diff : ");
+		Serial.println(CurDifAngle);
+		
+		LstDifAngle = CurDifAngle;
 		sLastime = sCurtime;
 	}
+	if(LstDifAngle != 0)
+		if(CurDifAngle < LstDifAngle)
+		{
+			digitalWrite(4, HIGH);
+			digitalWrite(5, LOW);
+		}
+		else if(CurDifAngle > LstDifAngle)
+		{
+			digitalWrite(4, LOW);
+			digitalWrite(5, HIGH);
+		}
 }
